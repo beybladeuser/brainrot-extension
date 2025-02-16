7cda66e2859110b13878e6c87b34faded33b5966
beybladeuser <rubencruz0312@gmail.com>
2025-02-16 23:21:57 +0000

    fixed bug with dragging too fast; fixed future problem with deleting the viewer messing up with counter; fixed position of drag bar

--- content.js
+++ content.js
@@ -1,25 +1,39 @@
 chrome.runtime.onMessage.addListener((message) => {
-    if (message.action === 'addDraggableContainer') {
-        addDraggableContainer();  // Call the function when the message is received
-    }
+	if (message.action === 'addDraggableContainer') {
+		addDraggableContainer();  // Call the function when the message is received
+	}
 });
 
 function addDraggableContainer() {
-	const count = $(".brainrotDraggableContainer").length;
-	console.log()
+	var count = $("#brainrotDraggableContainerCount").length;
+	if (count == 0) {
+		$("body").append(`<input id="brainrotDraggableContainerCount" type="hidden" value="0">`);
+	}
+	count = $("#brainrotDraggableContainerCount").val();
 
-    $("body").append(`
+	$("body").append(`
         <div id="brainrotDraggableContainer-${count}" class="brainrotDraggableContainer">
 		</div>
     `);
+
 	const draggableContainer = $(`#brainrotDraggableContainer-${count}`);
-    draggableContainer.draggable();
+
+	$("#brainrotDraggableContainerCount").val(parseInt(count) + 1)
+	draggableContainer.draggable();
 
 	fetch(chrome.runtime.getURL("brainrot-viewer.html"))
-        .then((response) => response.text())
-        .then((html) => {
-            draggableContainer.append(html);
-        })
-        .catch((err) => console.error("Error loading HTML:", err));
+		.then((response) => response.text())
+		.then((html) => {
+			draggableContainer.append(html);
+			const iframe = $(`#brainrotDraggableContainer-${count} iframe`);
+			draggableContainer.on("dragstart", () => {
+				iframe.css("pointer-events", "none");
+			});
+
+			draggableContainer.on("dragstop", () => {
+				iframe.css("pointer-events", "auto");
+			});
+		})
+		.catch((err) => console.error("Error loading HTML:", err));
 
 }
