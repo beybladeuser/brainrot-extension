6f6d7359463de2e7c5780d5618f0e54d3a96af8c
beybladeuser <rubencruz0312@gmail.com>
2025-02-17 10:59:26 +0000

    remove useless var

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,3 @@
 document.getElementById("addBrainrotViewer").addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
     chrome.runtime.sendMessage({ action: 'addDraggableContainer' });
 });
